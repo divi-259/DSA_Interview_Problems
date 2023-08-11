10baefb94d48c2c589d0fa09ebdfde576c588379
Divyanshi Dixit <divyanshi259dixit@gmail.com>
2023-08-10 19:36:16 -0700

    top view of a binary tree

---
+++ Binary_Trees/Top_View_Binary_Tree.java
@@ -0,0 +1,177 @@
+/*
+Problem Link: https://practice.geeksforgeeks.org/problems/top-view-of-binary-tree/1
+*/
+//{ Driver Code Starts
+//Initial Template for JAVA
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+public class Tree {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+    	    	
+                Solution ob = new Solution();
+
+                ArrayList<Integer> vec = ob.topView(root);
+                for(int x : vec)
+                    System.out.print(x + " ");
+                System.out.println();
+        	
+                t--;   
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+
+//User function Template for Java
+class Tuple {
+    Node node;
+    int col;
+    public Tuple(Node node, int col) {
+        this.node = node;
+        this.col = col; // col is like = -2 -1 0 +1 +2 = vertical ordering 
+    }
+}
+
+class Solution
+{
+    //Function to return a list of nodes visible from the top view 
+    //from left to right in Binary Tree.
+    static ArrayList<Integer> topView(Node root)
+    {
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        Queue<Tuple> q = new LinkedList<>();
+        q.add(new Tuple(root,0)); // root node is at 0th vertical
+
+        // we are traversing the tree in level order traversal and storing the nodes in our data structure
+        // we are replacing with the latest node found in that vertical
+        while(!q.isEmpty()) {
+            Tuple tp = q.remove();
+            Node node = tp.node;
+            int c = tp.col;
+        
+            // keep replacing the current vertical with the latest node value
+            if(!map.containsKey(c))
+                map.put(c, node.data);
+
+            if(node.left!=null) {
+                q.offer(new Tuple(node.left, c-1));
+            }
+            if(node.right!=null) {
+                q.offer(new Tuple(node.right, c+1));
+            }
+        }
+
+        // time to popoulate the answer
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int vertical : map.values()) {
+            ans.add(vertical);
+        }
+        return ans;
+        
+    }
+}
